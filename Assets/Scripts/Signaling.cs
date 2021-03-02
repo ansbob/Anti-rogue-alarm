943d0414d226920e9a028e81ab9efb0ee6689388
Ansar <theansbob@gmail.com>
2021-03-02 18:36:03 +0600

    release v1

---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private AudioSource _sound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Thief>(out Thief thief))
+        {
+            StartCoroutine(PlaySound());
+        }
+    }
+
+    private IEnumerator PlaySound()
+    {
+        _sound.volume = 0;
+
+        _sound.Play();
+
+        for (int i = 0; i < 100; i++)
+        {
+            if (i%10 == 0)
+                _sound.volume += 0.1f;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator StopSound()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            if (i % 10 == 0)
+                _sound.volume -= 0.1f;
+
+            Debug.Log(_sound.volume);
+
+            yield return null;
+        }
+
+        _sound.Stop();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent<Thief>(out Thief thief))
+        {
+            StartCoroutine(StopSound());
+        }
+    }
+}
---
+++ Assets/Scripts/Thief.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thief : MonoBehaviour
+{
+    [SerializeField] private int _speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-_speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(_speed * Time.deltaTime, 0, 0);
+        }
+    }
+}
